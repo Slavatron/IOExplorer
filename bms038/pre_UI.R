69ecf3a50fae6f9eb34437e72d77d9bd6918d9cd
Nadeem Riaz <nadeem.riaz@gmail.com>
2016-12-23 20:42:34 -0500

    Modifcations to Pre-Tab UI layout; some additional explanatory text added

--- bms038/pre_UI.R
+++ bms038/pre_UI.R
@@ -9,7 +9,7 @@ createPreTabUI <- function() {
   pre_ui <- sidebarLayout(
     sidebarPanel(
       wellPanel(
-        selectInput("genomicSpace", "Genomic Space:", varList),
+        selectInput("pre_genomicSpace", "Genomic Space:", varList),
         uiOutput("pre_Second"),
         uiOutput("pre_Slider")
       ),
@@ -18,9 +18,18 @@ createPreTabUI <- function() {
     
     # Show the caption and plot of the requested variable against mpg
     mainPanel(
-      h3(textOutput("pre_caption")),
-      uiOutput("CHOOSE_SURVIVAL_TYPE"),
-      plotOutput("survival_plot"),
+      h3("Pre-treatment Genomic Predictors & Outcomes"),
+      "Pre-treatment genomic predictors and their relationship with Overall Survival (OS), Progression Free Survival (PFS), 
+      and respone can be examined here. First select the type of genomic data you would like to look at \
+      on the left; then select the particular genomic feature from the menu below. For example, one can select exome \
+      and then pick mutation load to look at the relationship of mutation load and survival. Different cutpoints on mutation \
+      load can be reviewed by using the slider on the left \
+      ",
+      p(),
+      p(),
+      uiOutput("pre_choose_survival_type"),
+      p(),
+      plotOutput("pre_survival_plot"),
       tableOutput("pre_my_table")
     ) 
   )
--- bms038/pre_tab_logic.R
+++ bms038/pre_tab_logic.R
@@ -17,62 +17,54 @@ getPreId <- function(genomicSpace, feature) {
 
 createPreTab <- function(input,output, predat) {
 
-  formulaText <- reactive({
-    paste("Survival ~", input$pre_sec_var)
+  output$pre_Second = renderUI( {
+    selectInput("pre_sec_var", "Feature", choices = unname(pre_choice1[[input$pre_genomicSpace]]))
+  })
+
+  # SLIDER FOR DEFINING CUT-POINT
+  output$pre_Slider = renderUI({
+    # Figure out which variable to look up in raw data
+    tid <- getPreId(input$pre_genomicSpace, input$pre_sec_var)
+    if ( tid == -1 ) { return() }
+    #print(sprintf("pre_slider %s %s", input$pre_genomicSpace, input$pre_sec_var))
+    medv <- median(predat[,tid], na.rm = TRUE)
+    minv <- min(predat[,tid], na.rm = TRUE)
+    maxv <- max(predat[,tid], na.rm = TRUE)
+    #print(sprintf("min = %.2f max = %.2f med = %.2f", minv, maxv, medv))
+    sliderInput("pre_slide1", label = h3(paste(input$pre_sec_var)), min = minv, max = maxv, value = medv, round = FALSE)
+  })
+
+  # SLIDER-CONTROLLED HISTOGRAM 
+  output$pre_slide_hist <- renderPlot({
+    # Figure out which variable to look up in raw data
+    tid <- getPreId(input$pre_genomicSpace, input$pre_sec_var)
+    if ( tid == -1 ) { return() }
+    niceHist(predat[,tid], input$pre_sec_var, cutpoint=input$pre_slide1)
+  })
+
+  # SURVIVAL PLOTS
+  output$pre_choose_survival_type = renderUI( {
+    radioButtons("Survival_Type", label = "Outcome to Analyze: ",choices = list("Overall Survival" = 1, "Progression Free Survival" = 2), inline=TRUE, selected = 1)
   })
   
-output$pre_caption <- renderText( { formulaText() } )
-
-
-
-
-output$pre_Second = renderUI( {
-  selectInput("pre_sec_var", "Feature", choices = unname(pre_choice1[[input$genomicSpace]]))
-})
-
-# SLIDER FOR DEFINING CUT-POINT
-output$pre_Slider = renderUI({
-  # Figure out which variable to look up in raw data
-  tid <- getPreId(input$genomicSpace, input$pre_sec_var)
-  if ( tid == -1 ) { return() }
-  print(sprintf("pre_slider %s %s", input$genomicSpace, input$pre_sec_var))
-  medv <- median(predat[,tid], na.rm = TRUE)
-  minv <- min(predat[,tid], na.rm = TRUE)
-  maxv <- max(predat[,tid], na.rm = TRUE)
-  print(sprintf("min = %.2f max = %.2f med = %.2f", minv, maxv, medv))
-  sliderInput("pre_slide1", label = h3(paste(input$pre_sec_var)), min = minv, max = maxv, value = medv, round = FALSE)
-})
-
-# SLIDER-CONTROLLED HISTOGRAM
-output$pre_slide_hist <- renderPlot({
-  # Figure out which variable to look up in raw data
-  tid <- getPreId(input$genomicSpace, input$pre_sec_var)
-  if ( tid == -1 ) { return() }
-  niceHist(predat[,tid], input$pre_sec_var, cutpoint=input$pre_slide1)
-})
-
-# SURVIVAL PLOTS
-output$CHOOSE_SURVIVAL_TYPE = renderUI( {
-  radioButtons("Survival_Type", label = h3("Choose Survival Type for Analysis"),choices = list("Overall Survival" = 1, "Progression Free Survival" = 2), selected = 1)
-})
-output$survival_plot <- renderPlot({
-  #print("Trying to render OS")
-  tid <- getPreId(input$genomicSpace, input$pre_sec_var)
+  output$pre_survival_plot <- renderPlot({
+    #print("Trying to render OS")
+    tid <- getPreId(input$pre_genomicSpace, input$pre_sec_var)
         
-  survival_plot = clever_gg_surv(predat, "OSWK", "OS_event", tid, input$pre_slide1, "Overall Survival")
-  switch = input$Survival_Type
-  if (switch == 2 ) {
-    survival_plot = clever_gg_surv(predat, "PFSWK", "PFS_event", tid, input$pre_slide1, "Progression Free Survival")
-  }
-  survival_plot
-})
+    survival_plot = clever_gg_surv(predat, "OSWK", "OS_event", tid, input$pre_slide1, "Overall Survival")
+    switch = input$Survival_Type
+    if (switch == 2 ) {
+      survival_plot = clever_gg_surv(predat, "PFSWK", "PFS_event", tid, input$pre_slide1, "Progression Free Survival")
+    }
+    survival_plot
+  })
 
 
-# SUMMARY TABLE
-#output$pre_my_table <- renderTable({
-#  mini_table = predat[,c("Sample", input$sec_var, "PFS", "PFS_SOR", "OS", "OS_SOR", "myBOR" )]
-#  mini_table = mini_table[order(mini_table[,input$sec_var], decreasing = FALSE),]
-#  mini_table
-#  })
+  # SUMMARY TABLE
+  #output$pre_my_table <- renderTable({
+  #  mini_table = predat[,c("Sample", input$sec_var, "PFS", "PFS_SOR", "OS", "OS_SOR", "myBOR" )]
+  #  mini_table = mini_table[order(mini_table[,input$sec_var], decreasing = FALSE),]
+  #  mini_table
+  #  })
 
 }
