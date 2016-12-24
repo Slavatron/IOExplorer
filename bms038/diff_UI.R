c6ce53ac5b48822118b690d105ffebfcb00d9f20
Nadeem Riaz <nadeem.riaz@gmail.com>
2016-12-23 21:37:55 -0500

    Updated Diff Tab UI Now consistent with updates Slav made to pre-treatment UI tab

--- bms038/diff_UI.R
+++ bms038/diff_UI.R
@@ -1,7 +1,7 @@
 source("key_columns.R")
 
 createDiffTabUI <- function() {
-  varList <- names(pre_choice1)
+  varList <- names(diff_choice1)
   
   diff_ui <- sidebarLayout(
     sidebarPanel(
@@ -15,9 +15,17 @@ createDiffTabUI <- function() {
     
     # Show the caption and plot of the requested variable against mpg
     mainPanel(
-      h3(textOutput("diff_caption")),
-      plotOutput("diff_os_plot"),
-      plotOutput("diff_pfs_plot"),
+      h3("Change in Genomic Predictors on Therapy & Outcomes"),
+      "The change in a genomic predictor on immunotherapy (increase or decrease) and its relationship with Overall Survival (OS), Progression Free Survival (PFS), 
+      and respone can be examined here. First select the type of genomic data you would like to look at \
+      on the left; then select the particular genomic feature from the menu below. For example, one can select exome \
+      and then pick change in mutation load to look at the relationship of mutation load and survival.
+      ",
+      p(),
+      p(),
+      uiOutput("diff_choose_survival_type"),
+      p(),
+      plotOutput("diff_survival_plot"),
       tableOutput("diff_my_table")
     ) 
   )
--- bms038/diff_tab_logic.R
+++ bms038/diff_tab_logic.R
@@ -15,54 +15,52 @@ getDiffId <- function(genomicSpace, feature) {
 
 createDiffTab <- function(input,output, preondat) {
 
-  formulaText <- reactive({
-    paste("Survival ~", input$diff_sec_var)
+  output$diff_Second = renderUI( {
+    selectInput("diff_sec_var", "Feature", choices = unname(diff_choice1[[input$diff_genomicSpace]]))
+  })
+
+  # SLIDER FOR DEFINING CUT-POINT
+  output$diff_Slider = renderUI({
+    # Figure out which variable to look up in raw data
+    tid <- getDiffId(input$diff_genomicSpace, input$diff_sec_var)
+    if ( tid == -1 ) { return() }
+    print(sprintf("diff_slider %s %s", input$diff_genomicSpace, input$diff_sec_var))
+    medv <- median(preondat[,tid], na.rm = TRUE)
+    minv <- min(preondat[,tid], na.rm = TRUE)
+    maxv <- max(preondat[,tid], na.rm = TRUE)
+    print(sprintf("min = %.2f max = %.2f med = %.2f", minv, maxv, medv))
+    sliderInput("diff_slide1", label = h3(paste(input$diff_sec_var)), min = minv, max = maxv, value = medv, round = FALSE)
+  })
+
+  # SLIDER-CONTROLLED HISTOGRAM
+  output$diff_slide_hist <- renderPlot({
+    # Figure out which variable to look up in raw data
+    tid <- getDiffId(input$diff_genomicSpace, input$diff_sec_var)
+    if ( tid == -1 ) { return() }
+    print(sprintf("Make histogram, tid='%s' genomicSpace='%s', feature='%s'", input$tid, input$diff_genomicSpace, input$diff_sec_var))
+    niceHist(preondat[,tid], input$diff_sec_var, input$diff_slide1)
+  })
+
+  ## Survival Plots
+  output$diff_choose_survival_type = renderUI( {
+    radioButtons("diff_Survival_Type", label = "Outcome to Analyze: ",choices = list("Overall Survival (OS)" = 1, "Progression Free Survival (PFS)" = 2), inline=TRUE, selected = 1)
   })
   
-output$diff_caption <- renderText( { formulaText() } )
 
-
-
-
-output$diff_Second = renderUI( {
-  selectInput("diff_sec_var", "Feature", choices = unname(diff_choice1[[input$diff_genomicSpace]]))
-})
-
-# SLIDER FOR DEFINING CUT-POINT
-output$diff_Slider = renderUI({
-  # Figure out which variable to look up in raw data
-  tid <- getDiffId(input$diff_genomicSpace, input$diff_sec_var)
-  if ( tid == -1 ) { return() }
-  print(sprintf("diff_slider %s %s", input$diff_genomicSpace, input$diff_sec_var))
-  medv <- median(preondat[,tid], na.rm = TRUE)
-  minv <- min(preondat[,tid], na.rm = TRUE)
-  maxv <- max(preondat[,tid], na.rm = TRUE)
-  print(sprintf("min = %.2f max = %.2f med = %.2f", minv, maxv, medv))
-  sliderInput("diff_slide1", label = h3(paste(input$diff_sec_var)), min = minv, max = maxv, value = medv, round = FALSE)
-})
-
-# SLIDER-CONTROLLED HISTOGRAM
-output$diff_slide_hist <- renderPlot({
-  # Figure out which variable to look up in raw data
-  tid <- getDiffId(input$diff_genomicSpace, input$diff_sec_var)
-  if ( tid == -1 ) { return() }
-  print(sprintf("Make histogram, tid='%s' genomicSpace='%s', feature='%s'", input$tid, input$diff_genomicSpace, input$diff_sec_var))
-  niceHist(preondat[,tid], input$diff_sec_var, input$diff_slide1)
-})
-
-# SURVIVAL PLOTS
-output$diff_os_plot <- renderPlot({
-  tid <- getDiffId(input$diff_genomicSpace, input$diff_sec_var)
-  print(sprintf("Make OS, tid='%s' genomicSpace='%s', feature='%s'", tid, input$diff_genomicSpace, input$diff_sec_var))
-  clever_gg_surv(preondat, "OSWK", "OS_event", tid, input$diff_slide1, "Overall Survival")
-})
-
-output$diff_pfs_plot <- renderPlot({
-  tid <- getDiffId(input$diff_genomicSpace, input$diff_sec_var)
-  print(sprintf("Make PFS, tid='%s' genomicSpace='%s', feature='%s'", input$tid, input$diff_genomicSpace, input$diff_sec_var))
-  clever_gg_surv(preondat, "PFSWK", "PFS_event", tid, input$diff_slide1, "Progression Free Survival")
-})
-
+  output$diff_survival_plot <- renderPlot({
+    tid <- getDiffId(input$diff_genomicSpace, input$diff_sec_var)
+    
+    switch = input$diff_Survival_Type
+    if (switch == 1 ) {
+      mytit = paste("OS by", input$diff_sec_var)
+      survival_plot = clever_gg_surv(preondat, "OSWK", "OS_event", tid, input$diff_slide1, mytit)
+    } else {
+      mytit = paste("PFS by", input$diff_sec_var)
+      survival_plot = clever_gg_surv(preondat, "PFSWK", "PFS_event", tid, input$diff_slide1, mytit)
+    }
+    return(grid.draw(survival_plot))
+  })
+  
 # SUMMARY TABLE
 #output$diff_my_table <- renderTable({
 #  mini_table = predat[,c("Sample", input$sec_var, "PFS", "PFS_SOR", "OS", "OS_SOR", "myBOR" )]
